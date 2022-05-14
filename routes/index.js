862944bf101599c401f2a34d3c505e4a7c091f1a
andy.H <qqqq5953@gmail.com>
2022-05-14 10:12:01 +0800

    套用 express-ejs-extend

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' })
+})
+router.get('/post', function (req, res, next) {
+  res.render('post', { title: 'Express' })
+})
+router.get('/dashboard/article', function (req, res, next) {
+  res.render('dashboard/article', { title: 'Express' })
+})
+router.get('/dashboard/archives', function (req, res, next) {
+  res.render('dashboard/archives', { title: 'Express' })
+})
+router.get('/dashboard/categories', function (req, res, next) {
+  res.render('dashboard/categories', { title: 'Express' })
+})
+router.get('/dashboard/signup', function (req, res, next) {
+  res.render('dashboard/signup', { title: 'Express' })
+})
 
-module.exports = router;
+module.exports = router
