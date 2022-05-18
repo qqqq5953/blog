957df700034363e92923d6962c5c7f295b2b995d
andy.H <qqqq5953@gmail.com>
2022-05-18 19:38:55 +0800

    使用ajax刪除文章、前台畫面呈現、切換草稿及公開文章

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,72 @@
 var express = require('express')
 var router = express.Router()
 const firebaseAdminDb = require('../connection/firebase_admin')
+const striptags = require('striptags')
+
+const categoriesRef = firebaseAdminDb.ref('/categories/')
+const articlesRef = firebaseAdminDb.ref('/articles/')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
+  let categories = {}
+  const articles = []
+
+  const getCategories = async () => {
+    const snapshot = await categoriesRef.once('value')
+    categories = snapshot.val()
+  }
+
+  const getArticlesByUpdateTime = async () => {
+    const snapshot = await articlesRef.orderByChild('updateTime').once('value')
+    snapshot.forEach((childSnapshot) => {
+      if ('public' === childSnapshot.val().status) {
+        articles.push(childSnapshot.val())
+      }
+    })
+    articles.reverse()
+  }
+
+  const renderData = async () => {
+    await getCategories()
+    await getArticlesByUpdateTime()
+    res.render('index', {
+      title: 'Express',
+      categories,
+      articles,
+      striptags
+    })
+  }
+
+  renderData()
 })
-router.get('/post', function (req, res, next) {
-  res.render('post', { title: 'Express' })
+
+router.get('/post/:id', function (req, res, next) {
+  let categories = {}
+  let article = {}
+  const id = req.params.id
+
+  const getCategory = async () => {
+    const snapshot = await categoriesRef.once('value')
+    categories = snapshot.val()
+  }
+
+  const getArticle = async () => {
+    const snapshot = await articlesRef.child(id).once('value')
+    article = snapshot.val()
+  }
+
+  const renderData = async () => {
+    await getCategory()
+    await getArticle()
+    res.render('post', {
+      title: 'Express',
+      categories,
+      article,
+      striptags
+    })
+  }
+
+  renderData()
 })
 
 router.get('/dashboard/signup', function (req, res, next) {
