6f6f133e6cdca51d7227d7984b6ac2b9f068b602
andy.H <qqqq5953@gmail.com>
2022-05-22 18:25:40 +0800

    archives 新增 toast

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,13 +2,13 @@ const express = require('express')
 const router = express.Router()
 const firebaseAdminDb = require('../connection/firebase_admin')
 const striptags = require('striptags')
-let userName = ''
+let userName
 
 const categoriesRef = firebaseAdminDb.ref('/categories/')
 const articlesRef = firebaseAdminDb.ref('/articles/')
 const usersRef = firebaseAdminDb.ref('/users/')
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   const getUserName = async () => {
     const uid = req.session.uid
     const snapshot = await usersRef.child(uid).once('value')
@@ -25,54 +25,10 @@ router.get('/', (req, res, next) => {
   renderData()
 })
 
-router.get('/article/create', function (req, res, next) {
-  categoriesRef.once('value').then((snapshot) => {
-    const categories = snapshot.val()
-    res.render('dashboard/article', {
-      userName,
-      categories,
-      article: {}
-    })
-  })
-})
+router.get('/archives', function (req, res) {
+  const deletedArticle = req.flash('delete')[0]
+  console.log('deleteMessage', deletedArticle)
 
-router.get('/article/preview/:id', function (req, res, next) {
-  const id = req.params.id
-  let article
-  let categories
-
-  const categoriesShapshot = categoriesRef.once('value')
-  const articlesShapshot = articlesRef.child(id).once('value')
-
-  Promise.all([categoriesShapshot, articlesShapshot]).then((item) => {
-    categories = item[0].val()
-    article = item[1].val()
-
-    res.render('dashboard/preview', {
-      userName,
-      categories,
-      article
-    })
-  })
-})
-
-router.get('/article/:id', function (req, res, next) {
-  const id = req.params.id
-  let categories = {}
-
-  categoriesRef
-    .once('value')
-    .then((snapshot) => {
-      categories = snapshot.val()
-      return articlesRef.child(id).once('value')
-    })
-    .then((snapshot) => {
-      const article = snapshot.val()
-      res.render('dashboard/article', { userName, categories, article })
-    })
-})
-
-router.get('/archives', function (req, res, next) {
   let categories = {}
   const articles = []
   const status = req.query.status || 'public'
@@ -101,36 +57,15 @@ router.get('/archives', function (req, res, next) {
       categories,
       articles,
       striptags,
-      status
+      status,
+      deletedArticle
     })
   }
 
   renderData()
-
-  // categoriesRef
-  //   .once('value')
-  //   .then((snapshot) => {
-  //     categories = snapshot.val()
-  //     return articlesRef.orderByChild('updateTime').once('value')
-  //   })
-  //   .then((snapshot) => {
-  //     const articles = []
-  //     snapshot.forEach((childSnapshot) => {
-  //       articles.push(childSnapshot.val())
-  //     })
-  //     // Object.values(snapshot.val()).forEach((item) => {
-  //     //   articles.push(item)
-  //     // })
-  //     articles.reverse()
-  //     res.render('dashboard/archives', {
-  //       title: 'Express',
-  //       categories,
-  //       articles
-  //     })
-  //   })
 })
 
-router.get('/categories', function (req, res, next) {
+router.get('/categories', function (req, res) {
   categoriesRef.once('value', (snapshot) => {
     const categories = snapshot.val()
     const alertMessages = req.flash('info')
@@ -144,28 +79,66 @@ router.get('/categories', function (req, res, next) {
 })
 
 // articles
-router.post('/article/create', (req, res) => {
-  const data = req.body
-  const articleRef = articlesRef.push()
-  const key = articleRef.key
-  // const updateTime = Math.floor(Date.now() / 1000)
-  const updateTime = () => {
-    const today = new Date()
-    const dd = String(today.getDate()).padStart(2, '0')
-    const mm = String(today.getMonth() + 1).padStart(2, '0')
-    const yyyy = today.getFullYear()
-    return yyyy + '-' + mm + '-' + dd
-  }
+router
+  .route('/article/create')
+  .get((req, res) => {
+    categoriesRef.once('value').then((snapshot) => {
+      const categories = snapshot.val()
+      res.render('dashboard/article', {
+        userName,
+        categories,
+        article: {}
+      })
+    })
+  })
+  .post((req, res) => {
+    const data = req.body
+    const articleRef = articlesRef.push()
+    const key = articleRef.key
+    // const updateTime = Math.floor(Date.now() / 1000)
+    const updateTime = () => {
+      const today = new Date()
+      const dd = String(today.getDate()).padStart(2, '0')
+      const mm = String(today.getMonth() + 1).padStart(2, '0')
+      const yyyy = today.getFullYear()
+      return yyyy + '-' + mm + '-' + dd
+    }
 
-  data.id = key
-  data.updateTime = updateTime()
-  articleRef.set(data)
-  res.redirect(`/dashboard/article/${key}`)
+    data.id = key
+    data.updateTime = updateTime()
+    articleRef.set(data)
+    res.redirect(`/dashboard/article/${key}`)
 
-  console.log(data)
+    console.log(data)
+  })
+
+router.get('/article/:id', (req, res) => {
+  const id = req.params.id
+  const categoriesSnapshot = categoriesRef.once('value')
+  const articleSnapshot = articlesRef.child(id).once('value')
+
+  Promise.all([categoriesSnapshot, articleSnapshot]).then((snapshots) => {
+    const categories = snapshots[0].val()
+    const article = snapshots[1].val()
+
+    res.render('dashboard/article', { userName, categories, article })
+  })
 })
 
-router.post('/article/update/:id', function (req, res, next) {
+router.get('/article/preview/:id', (req, res) => {
+  const id = req.params.id
+  const categoriesShapshot = categoriesRef.once('value')
+  const articlesShapshot = articlesRef.child(id).once('value')
+
+  Promise.all([categoriesShapshot, articlesShapshot]).then((snapshots) => {
+    const categories = snapshots[0].val()
+    const article = snapshots[1].val()
+
+    res.render('dashboard/preview', { userName, categories, article })
+  })
+})
+
+router.post('/article/update/:id', (req, res) => {
   const id = req.params.id
   const data = req.body
 
@@ -179,10 +152,25 @@ router.post('/article/update/:id', function (req, res, next) {
 
 router.post('/article/delete/:id', (req, res) => {
   const id = req.params.id
-  articlesRef.child(id).remove()
 
-  res.send('文章已刪除')
-  res.end()
+  const getDeleteArticle = async () => {
+    const snapshot = await articlesRef.child(id).once('value')
+    const deletedArticle = snapshot.val()
+    req.flash('delete', { ...deletedArticle, message: '刪除成功' })
+  }
+
+  const deleteArticle = async () => {
+    articlesRef.child(id).remove()
+  }
+
+  const processRequest = async () => {
+    await getDeleteArticle()
+    await deleteArticle()
+    res.send('文章已刪除')
+    res.end()
+  }
+
+  processRequest()
 })
 
 // category
